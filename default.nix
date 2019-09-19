313e29da4038762a815a154a333c69fae235be9c
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-19 15:39:47 +0300

    Build image in this repository

--- default.nix
+++ default.nix
@@ -1,8 +1,64 @@
+{}:
+
 with import <nixpkgs> {
 #add_postfix_test
   overlays = [
     (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = "master"; }))
   ];
 };
+let
+  inherit (builtins) concatMap getEnv toJSON;
+  inherit (dockerTools) buildLayeredImage;
+  inherit (lib) concatMapStringsSep firstNChars flattenSet dockerRunCmd mkRootfs;
+  inherit (lib.attrsets) collect isDerivation;
+  inherit (stdenv) mkDerivation;
 
-php54-image
+  php54DockerArgHints = lib.phpDockerArgHints php.php54;
+
+  rootfs = mkRootfs {
+    name = "apache2-rootfs";
+    src = ./rootfs;
+    inherit curl coreutils findutils apacheHttpdmpmITK apacheHttpd
+      mjHttpErrorPages postfix s6 execline;
+    php54 = php.php54;
+    zendguard = zendguard.loader-php54;
+    zendopcache = phpPackages.php54Packages.zendopcache;
+    mjperl5Packages = mjperl5lib;
+    ioncube = ioncube.v54;
+    s6PortableUtils = s6-portable-utils;
+    s6LinuxUtils = s6-linux-utils;
+    mimeTypes = mime-types;
+    libstdcxx = gcc-unwrapped.lib;
+  };
+
+in
+
+pkgs.dockerTools.buildLayeredImage rec {
+  maxLayers = 124;
+  name = "docker-registry.intr/webservices/apache2-php54";
+  tag = "latest";
+  contents = [
+    rootfs
+    tzdata
+    locale
+    postfix
+    sh
+    coreutils
+    perl
+  ] ++ collect isDerivation phpPackages.php54Packages;
+  config = {
+    Entrypoint = [ "${rootfs}/init" ];
+    Env = [
+      "TZ=Europe/Moscow"
+      "TZDIR=${tzdata}/share/zoneinfo"
+      "LOCALE_ARCHIVE_2_27=${locale}/lib/locale/locale-archive"
+      "LOCALE_ARCHIVE=${locale}/lib/locale/locale-archive"
+      "LC_ALL=en_US.UTF-8"
+    ];
+    Labels = flattenSet rec {
+      ru.majordomo.docker.arg-hints-json = builtins.toJSON php54DockerArgHints;
+      ru.majordomo.docker.cmd = dockerRunCmd php54DockerArgHints "${name}:${tag}";
+      ru.majordomo.docker.exec.reload-cmd = "${apacheHttpd}/bin/httpd -d ${rootfs}/etc/httpd -k graceful";
+    };
+  };
+}
